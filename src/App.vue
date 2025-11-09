84b99e8819fccd13654f2f20892f531702bf33be
Sander Coemans <sander.coemans@student.ucll.be>
2025-11-09 14:20:19 +0100

    enabled routing properly

---
+++ src/App.vue
@@ -0,0 +1,7 @@
+<script setup lang="ts"/>
+
+<template>
+  <div class="w-full overflow-x-hidden">
+    <router-view />
+  </div>
+</template>
--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import './style.css'
 import router from './router'
-import Pokedex from './views/Pokedex.vue'
+import App from './App.vue'
 
 const pinia = createPinia()
-const app = createApp(Pokedex)
+const app = createApp(App)
 app.use(pinia)
 app.use(router)
 app.mount('#app')
