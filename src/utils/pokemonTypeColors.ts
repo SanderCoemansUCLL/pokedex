6205cdc8aa273848567b38d8635f53ef22106686
Sander Coemans <sander.coemans@student.ucll.be>
2025-11-09 03:56:11 +0100

    mapper for pokemon types to tailwind colors

---
+++ src/utils/pokemonTypeColors.ts
@@ -0,0 +1,21 @@
+const colours = {
+  normal: 'bg-gray-400',
+  fire: 'bg-orange-500',
+  water: 'bg-blue-500',
+  electric: 'bg-yellow-400',
+  grass: 'bg-green-500',
+  ice: 'bg-cyan-300',
+  fighting: 'bg-red-600',
+  poison: 'bg-purple-500',
+  ground: 'bg-yellow-600',
+  flying: 'bg-indigo-400',
+  psychic: 'bg-pink-500',
+  bug: 'bg-lime-500',
+  rock: 'bg-stone-500',
+  ghost: 'bg-indigo-700',
+  dragon: 'bg-indigo-600',
+  steel: 'bg-slate-400',
+  fairy: 'bg-pink-300',
+};
+
+export default colours;
