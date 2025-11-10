718129570c3083f80fbb2af18a99e809f19c9464
Sander Coemans <sander.coemans@student.ucll.be>
2025-11-10 18:03:04 +0100

    gradient bgs

--- src/views/PokemonDetails.vue
+++ src/views/PokemonDetails.vue
@@ -7,6 +7,8 @@ import colours from '../utils/pokemonTypeColors'
 import PokemonAbout from '../components/pokemon/PokemonAbout.vue'
 import PokemonStats from '../components/pokemon/PokemonStats.vue'
 import PokemonMoves from '../components/pokemon/PokemonMoves.vue'
+import { Heart } from 'lucide-vue-next';
+import { useFavouritesStore } from '../stores/favourites';
 
 const route = useRoute()
 
@@ -16,17 +18,31 @@ const { pokemon, isLoading, error } = usePokemonDetails(nameRef)
 
 const primaryType = computed(() => pokemon.value?.types?.[0]?.type?.name ?? 'normal')
 
-const bgColor = computed(() => colours[primaryType.value as keyof typeof colours])
+const colors = computed(() => colours[primaryType.value as keyof typeof colours]);
+
+const bgStyle = computed(() => ({
+  background: `linear-gradient(${colors.value[0]}, ${colors.value[1]})`
+}))
 
 const capitalizedName = computed(() => {
   const n = pokemon.value?.name ?? ''
   return n ? n.charAt(0).toUpperCase() + n.slice(1) : ''
 })
 
+const favouritesStore = useFavouritesStore();
+
+const toggleFavourite = () => {
+  favouritesStore.toggleFavourite(pokemon.value?.id ?? 0);
+}
+
+const isFavourite = computed(() => {
+  return favouritesStore.isFavourite(pokemon.value?.id ?? 0);
+});
+
 </script>
 
 <template>
-  <div :class="['min-h-screen min-w-screen', bgColor]" >
+  <div class="min-h-screen min-w-screen" :style="bgStyle">
     <div v-if="error">
       <p class="text-red-500">Error: {{ error }}</p>
     </div>
@@ -39,23 +55,25 @@ const capitalizedName = computed(() => {
       <p class="text-red-500">Pokemon not found.</p>
     </div>
 
-    <div v-else class="pt-8 pl-8">
+    <div v-else class="pt-8 pl-8 pr-8 flex justify-between items-baseline">
       <h1 class="text-left font-bold">{{ capitalizedName }}</h1>
+      <Heart v-if="isFavourite" :size="Number(32)" @click="toggleFavourite" fill="white"/>
+      <Heart v-else :size="Number(32)" @click="toggleFavourite" />
     </div>
     <PokemonLightBox :sprites="pokemon!.sprites" />
 
     <div class="pl-8 pr-8 pb-6">
-      <h2 class="text-left">About</h2>
+      <h2 class="text-left font-bold text-lg">About</h2>
       <PokemonAbout :pokemon="pokemon!" />
     </div>
 
     <div class="pl-8 pr-8 pb-6">
-      <h2 class="text-left">Stats</h2>
+      <h2 class="text-left font-bold text-lg">Stats</h2>
       <PokemonStats :stats="pokemon!.stats" />
     </div>
 
     <div class="pl-8 pr-8 pb-6">
-      <h2 class="text-left">Moveset</h2>
+      <h2 class="text-left font-bold text-lg">Moveset</h2>
       <PokemonMoves :moves="pokemon!.moves" />
     </div>
 
